acd013df6222b17251b403dc9e0867b41ff01d8e
RKBromen <tanconghiep2005@gmail.com>
2025-10-22 09:24:57 +0700

    add logout

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import Image from "next/image";
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Header() {
+    const [show, setShow] = useState(false);
+
+    function handleShow() {
+        setShow(!show);
+    }
+
     return (
         <div>
             <div className="bg-gray-300 flex w-full items-center">
@@ -11,12 +20,16 @@ export default function Header() {
                     <Image alt="bus" src="/busicon.png" height={50} width={100} className="me-2"></Image>
                 </Link>
                 <p className="font-bold text-xl">Smart School Bus Tracking System</p>
-                <div className='ml-auto mr-4'>
-                    <Link href="/user/information">
-                        <FontAwesomeIcon icon={faUser} className='fa-2xl' />
-                    </Link>
+                <div className='flex flex-col ml-auto mr-4 items-center'>
+                    <FontAwesomeIcon icon={faUser} className='fa-2xl' onClick={handleShow} />
+                    {show && <div className='bg-stone-50 flex flex-col'>
+                        <Link href="/user/information" className='hover:bg-stone-200'>
+                            Thông tin tài xế
+                        </Link>
+                        <div className='hover:bg-stone-200'>Log out</div>
+                    </div>}
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
